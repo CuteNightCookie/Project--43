ebbee5270393a7fe9a0fe073309e2b5e9d174eef
CuteNightCookie <73283797+CuteNightCookie@users.noreply.github.com>
2021-01-15 08:32:17 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+hr = hour();
+mn = minute();
+sc = seconds();
+
+angleMode(DEGREES);
+
+function map() {
+  scAngle = map(sc, 0, 60, 0, 360);
+}
+
+function line() {
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
